2f194cdc90c2b131978619ecc24193f37c848a38
digvijay <digvijaytest82@gmail.com>
2026-03-31 14:21:03 +0530

    new file

---
+++ src/Constant/Localvariables.java
@@ -0,0 +1,9 @@
+package Constant;
+
+public class Localvariables{
+    public static void main(String[] args) {
+        final int a = 22;
+         //a = 12;
+        System.out.println( a);
+    }
+}
