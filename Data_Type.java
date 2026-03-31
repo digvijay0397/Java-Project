9c69fc2f2bf9dfe9bbd917e93da9b7acf6d98410
digvijay <digvijaytest82@gmail.com>
2026-03-31 12:17:38 +0530

    first commit

---
+++ Data_Type.java
@@ -0,0 +1,24 @@
+public class Data_Type {
+    public static void main(String[] args) {
+        int I = 101;
+        System.out.println("Data_Type(int) = " + I);
+        float F = 101.1f;
+        System.out.println("Data_Type(float) = " + F);
+        double D = 101.1;
+        System.out.println("Data_Type(double) = " + D);
+        char C = 'A';
+        System.out.println("Data_Type(char) = " + C);
+        boolean B = true;
+        System.out.println("Data_Type(boolean) = " + B);
+        String S = "JJava Practice";
+        System.out.println("Data_Type(String) = " + S);
+        Long L = 101L;
+        System.out.println("Data_Type(Long) = " + L);
+        byte M = 19;
+        System.out.println("Data_Type(byte) = " + M);
+        short SH = 1234;
+        System.out.println("Data_Type(short) = " + SH);
+    
+    
+    }
+}
