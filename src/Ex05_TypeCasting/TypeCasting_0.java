ab074049eb9d29901d6015268fb4300d08ceeea4
digvijay <digvijaytest82@gmail.com>
2026-04-02 13:10:51 +0530

    demo commit

--- src/Ex05_TypeCasting/TypeCasting_0.java
+++ src/Ex05_TypeCasting/TypeCasting_0.java
@@ -3,6 +3,8 @@ package Ex05_TypeCasting;
 public class TypeCasting_0 {
     public static void main (String[] args){
         System.out.println(12);
+        System.out.println(18);
+        System.out.println(14);
 
 
     }
