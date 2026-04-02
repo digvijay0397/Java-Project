1d2541948b074bc201d13ff14712b7873397e1e0
digvijay <digvijaytest82@gmail.com>
2026-04-02 14:56:03 +0530

    02march_1stcommit

--- src/Ex05_TypeCasting/TypeCasting_0.java
+++ src/Ex05_TypeCasting/TypeCasting_0.java
@@ -2,9 +2,20 @@ package Ex05_TypeCasting;
 
 public class TypeCasting_0 {
     public static void main (String[] args){
-        System.out.println(12);
-        System.out.println(18);
-        System.out.println(14);
+      //int b = 210;
+      //int a = b; //valid syntax
+       // int a1 = (byte)b;
+      //  System.out.println(a1);
+
+        int cource = 100;
+        float NSRT_Tax = 18.45F;
+       // int total = cource + NSRT_Tax;  //Narrowing - Implicit.
+       // int total = cource + (int)NSRT_Tax;  //Narrowing - Explicit.
+
+        float total2 = cource + NSRT_Tax;  //widening - Implicit.
+        float total3 = (float) cource + NSRT_Tax; //widening - Explicit.
+        System.out.println(total2);
+
 
 
     }
