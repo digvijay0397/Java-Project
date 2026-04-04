6a2592b6bd6e112f3770e334df88c45608192ebf
Digvijay Singh <digvijaysng65@gmail.com>
2026-04-04 22:48:33 +0530

    4march-5st commit

---
+++ src/Ex07_Increment_Decrement_OP/LAB38_Post_Decrement.java
@@ -0,0 +1,11 @@
+package Ex07_Increment_Decrement_OP;
+
+public class LAB38_Post_Decrement {
+    public static void main(String[] args) {
+        int a = 10 ;
+        int result_post = a-- ;
+        System.out.println(result_post);
+        System.out.println(a);
+
+    }
+}
