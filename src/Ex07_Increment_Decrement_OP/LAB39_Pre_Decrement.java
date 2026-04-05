16d0e7e06c36f071443473516be0547fef0fdd68
Digvijay Singh <digvijaysng65@gmail.com>
2026-04-05 12:40:42 +0530

    5march-1st commit

---
+++ src/Ex07_Increment_Decrement_OP/LAB39_Pre_Decrement.java
@@ -0,0 +1,12 @@
+package Ex07_Increment_Decrement_OP;
+
+public class LAB39_Pre_Decrement {
+
+    public static void main (String[] args) {
+        int a = 30 ;
+        int result = --a ;
+
+        System.out.println(result);
+        System.out.println(a);
+    }
+}
---
+++ src/Task/LAB40_IncreANDDecre.java
@@ -0,0 +1,8 @@
+package Task;
+
+public class LAB40_IncreANDDecre {
+    public static void main (String[] args) {
+        int a = 10;
+        System.out.println();
+    }
+}
