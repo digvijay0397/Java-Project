48fd25ed9944266df21446fd1169cb2fc2b579a3
Digvijay Singh <digvijaysng65@gmail.com>
2026-04-05 14:01:56 +0530

    5april-3commit

---
+++ src/Ex08_If_Condition/LAB45_If_pt3.java
@@ -0,0 +1,12 @@
+package Ex08_If_Condition;
+
+public class LAB45_If_pt3 {
+    public static void main (String[] args) {
+        int age = 25;
+        if (age >= 25 ) {
+            System.out.println("yes, Lets go to Thailand!");
+        }else {
+            System.out.println("No, Stay here!");
+        }
+    }
+}
---
+++ src/Ex08_If_Condition/LAB46_If_else_scanner.java
@@ -0,0 +1,12 @@
+package Ex08_If_Condition;
+
+import java.util.Scanner;
+
+public class LAB46_If_else_scanner {
+    public static void main(String[] args) {
+
+        Scanner scanner =   new Scanner(System.in);
+        System.out.println("Enter the age!");
+        int age = scanner.nextInt   ();
+    }
+}
