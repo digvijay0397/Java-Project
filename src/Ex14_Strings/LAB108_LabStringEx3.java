29386150291c00c871dbc4eef6c75c84f1274f4b
digvijay <digvijaytest82@gmail.com>
2026-04-07 11:37:13 +0530

    7April-2commit

--- src/Ex14_Strings/LAB108_LabStringEx3.java
+++ src/Ex14_Strings/LAB108_LabStringEx3.java
@@ -1,4 +1,13 @@
 package Ex14_Strings;
 
 public class LAB108_LabStringEx3 {
+    public static void main(String[] args) {
+        String s = "DigVijay".substring(3);
+        System.out.println(s);
+
+//        "unhappy".substring(2) returns "happy"
+//        "Harbison".substring(3) returns "bison"
+//        "emptiness".substring(9) returns "" (an empty string)
+
+    }
 }
