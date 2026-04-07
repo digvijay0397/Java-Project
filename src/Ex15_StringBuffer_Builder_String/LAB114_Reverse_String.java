a68602014975cfebf24c4766dc6f0f0ad653ee6a
digvijay <digvijaytest82@gmail.com>
2026-04-07 13:08:05 +0530

    7April-5commit

---
+++ src/Ex15_StringBuffer_Builder_String/LAB114_Reverse_String.java
@@ -0,0 +1,16 @@
+package Ex15_StringBuffer_Builder_String;
+
+import java.util.Scanner;
+
+public class LAB114_Reverse_String {
+    public static void main(String[] args) {
+
+        Scanner scanner =new Scanner(System.in);
+        String input = scanner.next();
+        String reverse = "";
+        for (int i= input.length() -1; i >=0; i--){
+            reverse = reverse + input.charAt(i);
+        }
+        System.out.println(reverse);
+    }
+}
