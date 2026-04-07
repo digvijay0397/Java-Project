ab7f0cb1e3474e80df27479ad3b2cea26b6c85bb
digvijay <digvijaytest82@gmail.com>
2026-04-07 11:44:45 +0530

    7April-3commit

--- src/Ex14_Strings/LAB108_LabStringEx3.java
+++ src/Ex14_Strings/LAB108_LabStringEx3.java
@@ -9,5 +9,23 @@ public class LAB108_LabStringEx3 {
 //        "Harbison".substring(3) returns "bison"
 //        "emptiness".substring(9) returns "" (an empty string)
 
+        char[] arr = "Java".toCharArray();
+        System.out.println(arr);
+
+        boolean b = " ".isBlank();
+        System.out.println(b);
+
+        boolean b1 = "".isEmpty();
+        System.out.println(b1);
+
+        String s2 = "DS".repeat(4);
+        System.out.println(s2);
+
+        boolean b11 = "Java".equalsIgnoreCase("java");
+        System.out.println(b11);
+
+        long count = "a\nb\nc\na".lines().count();
+        System.out.println(count);
+
     }
 }
