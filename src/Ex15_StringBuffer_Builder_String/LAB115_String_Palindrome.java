b67ab01b0324d561646ae155a4a89326de10445d
digvijay <digvijaytest82@gmail.com>
2026-04-07 13:30:28 +0530

    7April-7commit

---
+++ src/Ex15_StringBuffer_Builder_String/LAB115_String_Palindrome.java
@@ -0,0 +1,25 @@
+package Ex15_StringBuffer_Builder_String;
+
+import java.util.Scanner;
+
+public class LAB115_String_Palindrome {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the String");
+        String user_input = scanner.next();
+
+        String reversed_string = "";
+
+        for (int i = user_input.length() -1; i>= 0 ; i--) {
+            reversed_string = reversed_string + user_input.charAt(i);
+        }
+
+        if (reversed_string.equalsIgnoreCase(user_input))  {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+
+    }
+}
