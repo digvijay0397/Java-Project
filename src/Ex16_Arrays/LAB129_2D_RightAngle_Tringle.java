ec159af8844ed5dda7bb6bfe01a6f322506317df
digvijay <digvijaytest82@gmail.com>
2026-04-08 14:31:27 +0530

    8April-2commit

---
+++ src/Ex16_Arrays/LAB129_2D_RightAngle_Tringle.java
@@ -0,0 +1,19 @@
+package Ex16_Arrays;
+
+import java.util.Scanner;
+
+public class LAB129_2D_RightAngle_Tringle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the n for the pattern, eg: 4");
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+    }
+
+}
