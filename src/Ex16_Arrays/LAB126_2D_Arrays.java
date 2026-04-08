a0152d7a4cec4e4e1f9527f7bc7b23c6e8d64e0e
digvijay <digvijaytest82@gmail.com>
2026-04-08 13:22:00 +0530

    8April-1commit

---
+++ src/Ex16_Arrays/LAB126_2D_Arrays.java
@@ -0,0 +1,18 @@
+package Ex16_Arrays;
+
+public class LAB126_2D_Arrays {
+    public static void main(String[] args) {
+        // 2D Array
+        int [] [] array_2d_old ={
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}       // -> 3*3
+        } ;
+        int [] [] array = {
+                {10, 20},
+                {30, 40},
+                {50, 60}        //  -> 3*2
+        } ;
+
+    }
+}
---
+++ src/Ex16_Arrays/LAB127_2D_p0.java
@@ -0,0 +1,13 @@
+package Ex16_Arrays;
+
+public class LAB127_2D_p0 {
+    public static void main(String[] args) {
+
+        int [][] matrix = {{1,2,3}, {4,5,6}, {7,8,9}};   //3*3
+        int [][] matrix_2 ={{1,2},{3,4},{5,6}};    // 2*2
+        int [][] matrix_3 ={{1},{3},{5}}; //3*1
+
+
+
+    }
+}
