fe4b2b74a39a28b36289eeaf4dd4baf3abe5a47c
digvijay <digvijaytest82@gmail.com>
2026-04-08 17:09:05 +0530

    8April-4commit

---
+++ src/Ex17_OOPs/LAB134_OOPs.java
@@ -0,0 +1,8 @@
+package Ex17_OOPs;
+
+public class LAB134_OOPs {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/Ex17_OOPs/Person.java
@@ -0,0 +1,25 @@
+package Ex17_OOPs;
+
+public class Person {
+
+    //Attributes
+    String name;
+    int age;
+    String gender;
+    int salary;
+    long phone_no;
+    String address;
+    String course_name;
+    String payment_id;
+    boolean isMale;
+    boolean isMarried;
+
+    // Behaviour
+    void speak(){}
+    void eat(){}
+    void sleep(){}
+    void doAssignment(){}
+    void watchRecordings(){}
+
+
+}
